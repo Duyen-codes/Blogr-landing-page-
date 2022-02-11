16728c19cd6f6ef08d6d6c99ed2e37f94b470243
Duyen Nguyen <hongduyen0705@gmail.com>
2022-02-11 11:35:33 +0200

    fixed nav

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Arrow icon rotates on navlink click
 
-const navItems = document.querySelectorAll(".nav-item");
+const navItems = [...document.querySelectorAll(".nav-item")];
 const navLinks = document.querySelectorAll(".nav-link");
 const arrowIcon = document.querySelector(".arrow-icon");
 const burgerIcon = document.querySelector(".burger-icon");
@@ -20,20 +20,34 @@ closeIcon.addEventListener("click", () => {
 });
 
 // Toggle between hiding and showing dropdown menu
-navItems.forEach((navItem) => {
-  navItem.addEventListener("click", (e) => {
-    navItems.forEach(navItem => {
-      navItem.classList.remove('active')
-      e.currentTarget.classList.add("active");
-    })
-  });
+// navItems.forEach((navItem) => {
+//   navItem.addEventListener("click", (e) => {
+//     navItems.forEach(navItem => {
+//       navItem.classList.remove('active')
+//       e.currentTarget.classList.add("active");
+//     })
+//   });
+// });
 
-});
+navItems.forEach(item => {
+  item.addEventListener('click', e => {
+    const clickedItem = e.target.closest('li')
+    const itemDetail = clickedItem.querySelector('.dropdown-menu');
+    const clickedItemActive = clickedItem.classList.contains('active')
+    clickedItem.classList.toggle('active')
+    if (clickedItemActive) {
+      itemDetail.style.display = 'none';
+    } else {
+
+      itemDetail.style.display = 'flex';
+    }
+  })
+})
 
 //Close the dropdown if the user clicks outside of it
-let dropDowns = document.querySelectorAll(".dropdown-menu");
-window.onclick = function (e) {
-  if (window.innerWidth >= 1200 && !dropDowns.contains(e.target) && !navItems.contains(e.target)) {
-    [dropDowns, navItems].forEach(each => each.classList.remove('active'))
-  }
-};
+// let dropDowns = document.querySelectorAll(".dropdown-menu");
+// window.onclick = function (e) {
+//   if (window.innerWidth >= 1200 && !dropDowns.contains(e.target) && !navItems.contains(e.target)) {
+//     [dropDowns, navItems].forEach(each => each.classList.remove('active'))
+//   }
+// };
