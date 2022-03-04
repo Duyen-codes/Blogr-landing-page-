56cc06205d47b1ba733c3d84a0d4055d7a3a6ca4
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-03-04 07:59:15 +0200

    fixed nav

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Arrow icon rotates on navlink click
-
+// Query Selectors
 const navItems = [...document.querySelectorAll(".nav-item")];
 const navLinks = document.querySelectorAll(".nav-link");
 const arrowIcon = document.querySelector(".arrow-icon");
 const burgerIcon = document.querySelector(".burger-icon");
 const closeIcon = document.querySelector(".close-icon");
 const navBar = document.querySelector(".navbar");
+const header = document.querySelector("header");
 
 burgerIcon.addEventListener("click", () => {
   burgerIcon.classList.add("open");
@@ -29,25 +29,25 @@ closeIcon.addEventListener("click", () => {
 //   });
 // });
 
-navItems.forEach(item => {
-  item.addEventListener('click', e => {
-    const clickedItem = e.target.closest('li')
-    const itemDetail = clickedItem.querySelector('.dropdown-menu');
-    const clickedItemActive = clickedItem.classList.contains('active')
-    clickedItem.classList.toggle('active')
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", (e) => {
+    const clickedItem = e.target.closest("li");
+    const itemDetail = clickedItem.querySelector(".dropdown-menu");
+    const clickedItemActive = clickedItem.classList.contains("active");
+    clickedItem.classList.toggle("active");
     if (clickedItemActive) {
-      itemDetail.style.display = 'none';
+      itemDetail.style.display = "none";
     } else {
-
-      itemDetail.style.display = 'flex';
+      itemDetail.style.display = "flex";
     }
-  })
-})
+  });
+});
 
-//Close the dropdown if the user clicks outside of it
-// let dropDowns = document.querySelectorAll(".dropdown-menu");
-// window.onclick = function (e) {
-//   if (window.innerWidth >= 1200 && !dropDowns.contains(e.target) && !navItems.contains(e.target)) {
-//     [dropDowns, navItems].forEach(each => each.classList.remove('active'))
-//   }
-// };
+header.addEventListener("click", (e) => {
+  console.log("header clicked");
+  if (!e.target.classList.contains("nav-link")) {
+    navItems.forEach((navItem) => {
+      navItem.classList.remove("active");
+    });
+  }
+});
